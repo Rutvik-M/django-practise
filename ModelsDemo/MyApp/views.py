43a0d6bb6f41687388834907925d511bc4c6edc8
Rutvik Manthalkar <manthalkar.rutvik@gmail.com>
2026-05-13 13:43:29 +0530

    added addcourses file

--- ModelsDemo/MyApp/views.py
+++ ModelsDemo/MyApp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Course
+from django.http import  HttpResponse
 
-# Create your views here.
+def addCourse(request):
+    if request.method=="GET":
+        return render(request,"addCourse.html",{})
+    else:
+        cname=request.POST["cname"]
+        duration=request.POST["duration"]
+        fees_details=request.POST["fees_details"]
+        c1=Course(cname=cname,duration=duration,fees_details=fees_details)
+        c1.save()
+        return HttpResponse("Record Added")
+
+    
